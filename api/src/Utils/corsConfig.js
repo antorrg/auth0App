35f6cd7e3c30fe879db7c8fff7eaf7ffc8f78945
antorrg <114789901+antorrg@users.noreply.github.com>
2024-04-07 10:23:14 -0300

    correccion cors

--- api/src/Utils/corsConfig.js
+++ api/src/Utils/corsConfig.js
@@ -6,13 +6,13 @@ const corsConfig = (req, res, next) => {
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization, x-access-token');
   res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
 
-  if (req.method === 'OPTIONS') {
-    // Preflight request, respond with 200 OK
-    res.status(200).end();
-  } else {
+  // if (req.method === 'OPTIONS') {
+  //   // Preflight request, respond with 200 OK
+  //   res.status(200).end();
+  // } else {
     // Pass to next middleware
     next();
-  }
+  //}
 };
 
 module.exports = corsConfig;
